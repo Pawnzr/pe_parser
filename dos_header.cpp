e015172e924a4c0790f3a0b01195fa11cfb5fc3b
Pawn3r <anduu150620@gmail.com>
2024-07-17 16:46:50 +0700

    parsing header

--- dos_header.cpp
+++ dos_header.cpp
@@ -31,8 +31,33 @@ int main() {
     std::cout << "Machine: " << std::hex << ntHeaders.FileHeader.Machine << std::endl;
     std::cout << "NumberOfSections: " << std::dec << ntHeaders.FileHeader.NumberOfSections << std::endl;
     
+    IMAGE_OPTIONAL_HEADER& optionalHeader = ntHeaders.OptionalHeader;
+    // if (optionalHeader.Magic != IMAGE_NT_OPTIONAL_HDR32_MAGIC && 
+    //     optionalHeader.Magic != IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
+    //     std::cerr << "Optional Header không hợp lệ!" << std::endl;
+    //     file.close();
+    //     return 1;
+    // }
     
-    
+    printf("\n******* FILE HEADER *******\n");
+    printf("\tMachine: 0x%x\n", ntHeaders.FileHeader.Machine);
+    printf("\tNumberOfSections: %d\n", ntHeaders.FileHeader.NumberOfSections);
+    printf("\tTimeDateStamp: %d\n", ntHeaders.FileHeader.TimeDateStamp);
+    printf("\tPointerToSymbolTable: 0x%x\n", ntHeaders.FileHeader.PointerToSymbolTable);
+    printf("\tNumberOfSymbols: 0x%x\n", ntHeaders.FileHeader.NumberOfSymbols);
+    printf("\tSizeOfOptionalHeader: 0x%x\n", ntHeaders.FileHeader.SizeOfOptionalHeader);
+    printf("\tCharacteristics: 0x%x\n", ntHeaders.FileHeader.Characteristics);
+    printf("\n******* DATA DIRECTORIES *******\n");
+    printf("\tExport Directory Address: 0x%x; Size: 0x%x\n", ntHeaders.OptionalHeader.DataDirectory[0].VirtualAddress, ntHeaders.OptionalHeader.DataDirectory[0].Size);
+    printf("\tExport Directory Address: 0x%x; Size: 0x%x\n", ntHeaders.OptionalHeader.DataDirectory[1].VirtualAddress, ntHeaders.OptionalHeader.DataDirectory[1].Size);
+
+	printf("\n******* OPTIONAL HEADER *******\n");
+    std::cout << "\tIMAGE_NT_OPTIONAL_HDR32_MAGIC: " << std::hex << IMAGE_NT_OPTIONAL_HDR_MAGIC << std::endl;
+    std::cout << "\tIMAGE_NT_OPTIONAL_HDR32_MAGIC: " << std::hex << IMAGE_NT_OPTIONAL_HDR64_MAGIC << std::endl;
+    std::cout << "\tMagic Number: " << std::hex << optionalHeader.Magic << std::endl;
+    std::cout << "\tAddressOfEntryPoint: " << std::hex << optionalHeader.AddressOfEntryPoint << std::endl;
+    std::cout << "\tImageBase: " << std::hex << optionalHeader.ImageBase << std::endl;
+    std::cout << "\tSizeOfImage: " << std::dec << optionalHeader.SizeOfImage << std::endl;
     file.close();
     return 0;
 
